73301b90131950a6983672c3249fe86e5b00f6f2
Yurii <oniks9821@gmail.com>
2024-11-22 07:56:55 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/landing-page-2.0/',
+    base: '/111/',
     build: {
       sourcemap: true,
       rollupOptions: {
